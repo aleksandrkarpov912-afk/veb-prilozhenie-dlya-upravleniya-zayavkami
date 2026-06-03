75cc10e04504866bd49ed7308d3199d1538a8c40
aleksandrkarpov912-afk <aleksandrkarpov912@gmail.com>
2026-06-03 13:30:25 +0300

    fix cors + socket gateway

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,25 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JwtModule } from './jwt/jwt.module';
-import { TicketsService } from './tickets/tickets.service';
 import { TicketsModule } from './tickets/tickets.module';
 import { MessagesModule } from './messages/messages.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, JwtModule, TicketsModule, MessagesModule],
+  imports: [
+    EventEmitterModule.forRoot(), // 👈 добавили event emitter
+    AuthModule,
+    PrismaModule,
+    JwtModule,
+    TicketsModule,
+    MessagesModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, TicketsService],
-  
+  providers: [AppService],
 })
 export class AppModule {}
